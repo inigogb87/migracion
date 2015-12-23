dd557306b93270598636c58021220db93247be12
Curso <Curso@GBRB4.ipartekaula.com>
2015-12-23 12:47:34 +0100

    migracion cambios initial import

---
+++ src/alumnosMain/Migracion.java
@@ -0,0 +1,22 @@
+package alumnosMain;
+
+import java.io.FileReader;
+
+public class Migracion {
+
+	static int conInsercciones = 0;
+	static int conErrores = 0;
+	static final String FICHERO_PERSONAS = "data/personas10.txt";
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Comenzamos migracion...");
+		
+		//leer fichero data/personas10.txt y mostrar por pantalla
+	
+	try{
+	FileReader fichero = new FileReader(FICHERO_PRUEBAS);
+	FileWriter outputStream = new FileWriter ("")
+
+}
+}
